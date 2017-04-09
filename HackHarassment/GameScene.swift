c52ba1ab98a19c04691cedd115a9fcaac392371d
Alina Abidi <aabidi@usc.edu>
2017-04-08 21:07:14 -0700

    scroll layers and stuff

--- HackHarassment/GameScene.swift
+++ HackHarassment/GameScene.swift
@@ -1,15 +1,69 @@
 import SpriteKit
 
 class GameScene: SKScene {
+     var hero: SKSpriteNode!
+     var scrollLayer: SKNode!
+     var sinceTouch : TimeInterval = 0
+     let fixedDelta: TimeInterval = 1.0/60.0 /* 60 FPS */
+     let scrollSpeed: CGFloat = 160
+    
     override func didMove(to view: SKView) {
         /* Set up your scene here */
+        
+        /* Recursive node search for 'hero' (child of referenced node) */
+        hero = self.childNode(withName: "//hero") as! SKSpriteNode
+        
+        /* Set reference to scroll layer node */
+        scrollLayer = self.childNode(withName: "scrollLayer")
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         /* Called when a touch begins */
+        
+        hero.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 250))
+        
+        /* Apply subtle rotation */
+        hero.physicsBody?.applyAngularImpulse(1)
+        
+        /* Reset touch timer */
+        sinceTouch = 0
+        
+        /* Play SFX */
+        let flapSFX = SKAction.playSoundFileNamed("sfx_flap", waitForCompletion: false)
+        self.run(flapSFX)
     }
     
     override func update(_ currentTime: TimeInterval) {
         /* Called before each frame is rendered */
+        
+        /* Grab current velocity */
+        let velocityY = hero.physicsBody?.velocity.dy ?? 0
+        
+        /* Check and cap vertical velocity */
+        if velocityY > 400 {
+            hero.physicsBody?.velocity.dy = 400
+        }
+        
+        /* Apply falling rotation */
+        if sinceTouch > 0.1 {
+            let impulse = -20000 * fixedDelta
+            hero.physicsBody?.applyAngularImpulse(CGFloat(impulse))
+        }
+        
+        /* Clamp rotation */
+        hero.zRotation = hero.zRotation.clamped(CGFloat(-20).degreesToRadians(), CGFloat(30).degreesToRadians())
+        hero.physicsBody!.angularVelocity = hero.physicsBody!.angularVelocity.clamped(-2, 2)
+        
+        /* Update last touch timer */
+        sinceTouch+=fixedDelta
+        
+        /* Process world scrolling */
+        scrollWorld()
+        
+    }
+    
+    func scrollWorld() {
+        /* Scroll World */
+        scrollLayer.position.x -= scrollSpeed * CGFloat(fixedDelta)
     }
 }
