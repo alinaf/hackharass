312f3c5ac1e4f63f2bd0f8ae9a885ab4e95f445c
Alina Abidi <aabidi@usc.edu>
2017-04-08 21:31:37 -0700

    fixed scrolling

--- HackHarassment/GameScene.swift
+++ HackHarassment/GameScene.swift
@@ -65,5 +65,22 @@ class GameScene: SKScene {
     func scrollWorld() {
         /* Scroll World */
         scrollLayer.position.x -= scrollSpeed * CGFloat(fixedDelta)
+        /* Loop through scroll layer nodes */
+        for ground in scrollLayer.children as! [SKSpriteNode] {
+            
+            /* Get ground node position, convert node position to scene space */
+            let groundPosition = scrollLayer.convert(ground.position, to: self)
+            
+            /* Check if ground sprite has left the scene */
+            if groundPosition.x <= -ground.size.width / 2  - self.size.width / 2{
+                
+                /* Reposition ground sprite to the second starting position */
+                let newPosition = CGPoint( x: (self.size.width / 2) + ground.size.width/2, y: groundPosition.y)
+                
+                /* Convert new node position back to scroll layer space */
+                ground.position = self.convert(newPosition, to: scrollLayer)
+            }
+        }
+        
     }
 }
